86bc437c0072727b253e969f4b15e37a50041e49
Aaron Sanabria <aaronsm01@proton.me>
2026-04-15 16:33:58 -0600

    linear interpolation from a uniform grid added

--- src/drivers/test_coeff.py
+++ src/drivers/test_coeff.py
@@ -1,6 +1,7 @@
 import torch
 from scipy.constants import e
 import matplotlib.pyplot as plt
+from cherab.core.atomic import hydrogen
 import sys
 import os
 
@@ -10,31 +11,35 @@ from modules import sigmav
 
 filepath = "../data/" + sys.argv[1]
 
-sigma = sigmav(filepath)
+sigma = sigmav(filepath, hydrogen)
 
-T_min = 10**sigma.lTadf11_t[0]
-T_max = 10**sigma.lTadf11_t[-1]
+lT_min = sigma.lTadf11[0]
+lT_max = sigma.lTadf11[-1]
 
-T_test = torch.linspace(T_min * 1.01, T_max * 0.99, 2000, device='cpu', dtype=torch.float64, requires_grad=True)
+T_test = torch.pow(torch.linspace(lT_min, lT_max, 100), 10)
 
 R_scaled = sigma.rates(T_test)
 
-loss = R_scaled.mean()
-loss.backward()
+#loss = R_scaled.mean()
+#loss.backward()
 
-print("R_scaled mean:", R_scaled.mean().item())
-print("R_scaled std :", R_scaled.std().item())
-print("Grad mean:", T_test.grad.abs().mean().item())
-print("Grad max :", T_test.grad.abs().max().item())
-print("Finite R:", torch.isfinite(R_scaled).all().item())
-print("Finite grad:", torch.isfinite(T_test.grad).all().item())
-print("logT range:", torch.log10(T_test).min().item(), torch.log10(T_test).max().item())
+#print("R_scaled mean:", R_scaled.mean().item())
+#print("R_scaled std :", R_scaled.std().item())
+#print("Grad mean:", T_test.grad.abs().mean().item())
+#print("Grad max :", T_test.grad.abs().max().item())
+#print("Finite R:", torch.isfinite(R_scaled).all().item())
+#print("Finite grad:", torch.isfinite(T_test.grad).all().item())
+#print("logT range:", torch.log10(T_test).min().item(), torch.log10(T_test).max().item())
 
+print(T_test)
+print(R_scaled)
 plt.title(r'Adimentional $\langle \sigma v \rangle$ rates')
 plt.xlabel(r'Normalized temperature $\hat{T}$')
 plt.ylabel(r'$\frac{\langle \sigma v \rangle(T_{max}\hat{T})}{\langle \sigma v \rangle(T_{max})}$')
-plt.scatter(sigma.lTadf11_t.detach().cpu().numpy(), (sigma.lradf11_1D_t - sigma.lradf11_1D_t[-1].detach()).detach().cpu().numpy(), label=f'adf11 for {filepath}')
+plt.plot(sigma.lTadf11.detach().cpu().numpy(), sigma.lrates_uniform.detach().cpu().numpy(), label=f'log rates for file={sys.argv[1]}')
 plt.plot(torch.log10(T_test).detach().cpu().numpy(), torch.log10(R_scaled).detach().cpu().numpy(), label="linear interpolation for test tensor")
+plt.xlim(lT_min.detach().cpu(), lT_max.detach().cpu())
+plt.ylim(sigma.lrates_uniform[0].detach().cpu(), sigma.lrates_uniform[-1].detach().cpu() + 1)
 plt.legend()
 plt.show()
 
--- src/modules/rate_coeff.py
+++ src/modules/rate_coeff.py
@@ -3,6 +3,9 @@
 # -------------------------------
 import numpy as np
 import torch
+
+import torch.nn.functional as F
+from scipy.interpolate import RegularGridInterpolator
 from cherab.core.atomic import hydrogen
 from cherab.openadas.parse.adf11 import parse_adf11
 
@@ -14,40 +17,60 @@ sys.path.append(os.path.abspath('../../'))
 # -------------------------------
 # Class
 # -------------------------------
+
 class sigmav:
-    def __init__(self, filepath, dtype=torch.float64, device='cpu', epsilon=1e-40):
-        self.data = parse_adf11(hydrogen, filepath)
+    def __init__(self, filepath, gas, dtype=torch.float64, device='cpu', epsilon=1e-40):
+        self.data = parse_adf11(gas, filepath)
         self.dtype = dtype
         self.device = device
         self.epsilon = epsilon
-        self.lTadf11_t = self._to_tensor(self._extract_data('te'))
-        self.lradf11_1D_t = self._to_tensor(self._extract_data('rates')).mean(dim=0)
-        self.rate_ref = 10 ** self.lradf11_1D_t.mean()
+        self.lrates_uniform, self.lTadf11 = self._uniform_interpolation()
 
     def _extract_data(self, value):
         key = list(self.data.keys())[0]
-        return self.data[key][1][value]
+        return np.array(self.data[key][1][value], dtype=np.float64)
 
-    def _to_tensor(self, x):
-        return torch.as_tensor(x, dtype=self.dtype, device=self.device)
+    def _uniform_interpolation(self):
+        lden = self._extract_data('ne')
+        ltemp = self._extract_data('te')
+        lrates = self._extract_data('rates')
 
+        interp = RegularGridInterpolator((lden, ltemp), lrates)
+        ln = np.linspace(lden.min(), lden.max(), 1000)
+        lt = np.linspace(ltemp.min(), ltemp.max(),1000)
+        X, Y = np.meshgrid(ln, lt, indexing='ij')
+        points = np.column_stack((X.ravel(), Y.ravel()))
+        lr = interp(points).reshape(len(ln),len(lt)).mean(axis=0)
+
+        return torch.as_tensor(lr, dtype=self.dtype, device=self.device), torch.as_tensor(lt, dtype = self.dtype, device=self.device)
 
     def _interp_lrate(self, ly):
-        ly = ly.clamp(self.lTadf11_t[0] + 1e-12, self.lTadf11_t[-1] - 1e-12)
-        idx = (torch.searchsorted(self.lTadf11_t, ly) - 1).clamp(0, self.lTadf11_t.size(0))
+        shape = ly.shape
+        ly_flat = ly.reshape(-1)
 
-        x0 = self.lTadf11_t[idx]
-        x1 = self.lTadf11_t[idx + 1]
+        idx = (torch.searchsorted(self.lTadf11, ly_flat) - 1).clamp(0, self.lTadf11.size(0) - 2)
+        x0 = self.lTadf11[idx]
+        x1 = self.lTadf11[idx + 1]
+        w = (ly_flat - x0) / (x1 - x0)
+        y0 = self.lrates_uniform[idx]
+        y1 = self.lrates_uniform[idx + 1]
+        out = (1 - w) * y0 + w * y1
+        return out.reshape(shape)
+        """
+        ly = ly.clamp(self.lTadf11[0] + 1e-12, self.lTadf11[-1] - 1e-12)
+        idx = (torch.searchsorted(self.lTadf11, ly) - 1).clamp(0, self.lTadf11.size(0) - 2)
+
+        x0 = self.lTadf11[idx]
+        x1 = self.lTadf11[idx + 1]
         w = (ly - x0) / (x1 - x0)
-        y0 = self.lradf11_1D_t[idx]
-        y1 = self.lradf11_1D_t[idx + 1]
+        y0 = self.lrates_uniform[idx]
+        y1 = self.lrates_uniform[idx + 1]
 
         return (1 - w) * y0 + w * y1
-
+        """
     def rates(self, T):
         T = T.clamp(min=self.epsilon)
         lT = torch.log10(T)
         lrate = self._interp_lrate(lT)
 
-        return 10 ** lrate / self.rate_ref
-
+        return 10 ** lrate
